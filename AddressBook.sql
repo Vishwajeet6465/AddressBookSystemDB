26f3c328d261ffff87a8622789cb561dd9c3fbd0
Vishwajeet Bamane <vishwajeet.bamane@nmiet.edu.in>
2024-09-30 17:11:29 +0530

    Added:Created table in datebase

--- AddressBook.sql
+++ AddressBook.sql
@@ -2,3 +2,16 @@ CREATE DATABASE address_book_system;
 USE address_book_system;
 SELECT database();
 
+CREATE TABLE AddressBookSystem(
+id INT AUTO_INCREMENT PRIMARY KEY,
+firstname VARCHAR(50),
+lastname VARCHAR(50),
+address VARCHAR(100),
+city VARCHAR(50),
+state VARCHAR(50),
+zip INT,
+phoneNo VARCHAR(12),
+email VARCHAR(100)
+);
+
+describe AddressBookSystem;
