52933a298a965b94b39568c890f073e3daac8a6c
Vishwajeet Bamane <vishwajeet.bamane@nmiet.edu.in>
2024-10-01 11:09:00 +0530

    Added:Delete contact fron table

--- AddressBook.sql
+++ AddressBook.sql
@@ -28,4 +28,9 @@ SELECT * FROM AddressBookSystem;
 
 SET SQL_SAFE_UPDATES = 0;
 UPDATE AddressBookSystem SET address = "Punavle", city = "Pune", state = "Maharastra", zip = "411342" WHERE firstname = "pallavi" AND lastname = "chikhale";
+
+DELETE FROM AddressBookSystem WHERE firstname = "tararani";
 SET SQL_SAFE_UPDATES = 1;
+
+SELECT * FROM AddressBookSystem;
+
